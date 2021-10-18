7004239aaa3f2924c505113fabe211862a7fa04c
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-18 18:55:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace checking_age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+            if(age>18)
+            {
+                Console.WriteLine("ALLOWED FOR VOTE");
+
+            }
+            else
+            {
+                Console.WriteLine("not allowed");
+
+            }
+        }
+    }
+}
